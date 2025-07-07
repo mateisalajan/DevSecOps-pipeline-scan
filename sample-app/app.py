14592f92f545cf0649303499237c6e80a964c55c
Matei Salajan <sergiusalajan42@gmail.com>
2025-07-07 12:45:35 +0300

    Helm-deployed app to K8's with :vuln and :secure images

--- sample-app/app.py
+++ sample-app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, world! (Intentionally vulnerable demo app)"
+    return "Hello, world! (Secure demo app)"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
